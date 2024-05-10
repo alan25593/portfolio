f10ed7a17d8528f243aeaf46857fa5b8a507aa9f
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-05-10 14:43:59 -0300

    Fixes
    Se corrigio el nav toogle colgado, se corrigio animación en experiencia, se arreglo el mensaje de error colgado del form y se agrego validaciones en el mensaje del formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById('nav-toggle'),
+          nav = document.getElementById('nav-menu');
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');            
+        });
     }
 }
-showMenu('nav-toggle','nav-menu')
+
+showMenu('nav-toggle','nav-menu');
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -24,22 +25,26 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
-  sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href="#' + sectionId + '"]');
         
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }                                                    
-    })
+        if (sectionsClass) { 
+            if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                sectionsClass.classList.add('active-link');
+            } else {
+                sectionsClass.classList.remove('active-link');
+            }                                                    
+        }
+    });
+    
 }
+
 window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
@@ -54,7 +59,7 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__item, .contact__input',{interval: 200}); 
 
 
 
@@ -80,12 +85,17 @@ function validarFormulario() {
         return false;
     }
 
+    // Validar el campo de mensaje
+    if (consulta.length < 10) {
+        mostrarMensajeError("El mensaje debe tener al menos 10 caracteres.");
+        return; // Detener el proceso si el mensaje es demasiado corto
+    }
+    // Validar el campo de correo electrónico
     var formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!formatoCorreo.test(correo)) {
-        mostrarMensajeError("Por favor, introduce un correo electrónico válido");
-        return false;
+        mostrarMensajeError("Por favor, introduce un correo electrónico válido.");
+        return; // Detener el proceso si el correo electrónico no es válido
     }
-
     ocultarMensajes();
     return true;
 }
@@ -122,13 +132,15 @@ function mostrarMensajeError(mensaje) {
     mensajeError.style.display = "block";
     // Ocultar el mensaje de error después de 5 segundos
     setTimeout(function() {
-        mensajeExito.style.display = "none";
+        mensajeError.style.display = "none"; // Aquí se corrige el error
     }, 3000);
 }
 
 function ocultarMensajes() {
-    document.getElementById("mensaje-exito").style.display = "none";
-    document.getElementById("mensaje-error").style.display = "none";
+    var mensajeExito = document.getElementById("mensaje-exito");
+    var mensajeError = document.getElementById("mensaje-error");
+    mensajeExito.style.display = "none";
+    mensajeError.style.display = "none";
 }
 
 function limpiarCampos() {
